baf81440f61c17bc7507e3493c48ef21f7ff6fa7
hanseungyoo <trustyoo86@gmail.com>
2022-07-18 23:35:21 +0900

    day-2: 기본 속성 및 event listen 관련 내용 참고

---
+++ src/components/EventListen.vue
@@ -0,0 +1,41 @@
+<template>
+  <p>{{ counter + counter2 }}</p>
+  <button @click="counter++">클릭하면 숫자가 올라갑니다.</button>
+  <button @click="onClick"></button>
+</template>
+
+<script lang="ts">
+  import { ref } from 'vue';
+
+  export default {
+    data() {
+      return {
+        counter2: 0,
+      };
+    },
+    setup() {
+      let counter = ref(0);
+
+      const onClick = (evt: MouseEvent) => {
+        if (evt) {
+          evt.preventDefault();
+          counter.value++;
+        }
+      };
+
+      return {
+        counter,
+        onClick,
+      };
+    },
+    methods: {
+      onClick2: (evt: MouseEvent) => {
+        if (evt) {
+          evt.preventDefault();
+        }
+
+        this.counter2++;
+      }
+    },
+  }
+</script>
