30fb8b185b9414e89dbbe29b3cf45e1cab7cdf00
HanseungYoo <hanseung.yoo@hanwha.com>
2022-07-19 23:18:45 +0900

    (day-3): computed, reactive valuables

---
+++ src/components/Computed.vue
@@ -0,0 +1,56 @@
+<template>
+  <h2>Small Items</h2>
+  <p v-for="item in small_items_c" :key="item.id">{{ item.text }}</p>
+  <p v-for="item in small_items_o" :key="item.id">{{ item.text }}</p>
+  <h2>Big Items</h2>
+  <p v-for="item in big_items_c" :key="item.id">{{ item.text }}</p>
+  <p v-for="item in big_items_o" :key="item.id">{{ item.text }}</p>
+</template>
+
+<script lang="ts">
+import { reactive, computed } from 'vue';
+
+export default {
+  data() {
+    return {
+      arr: [
+        { id: 1, text: 'option item 1' },
+        { id: 2, text: 'option item 2' },
+        { id: 3, text: 'option item 3' },
+        { id: 4, text: 'option item 4' },
+        { id: 5, text: 'option item 5' },
+      ],
+    };
+  },
+  computed: {
+    small_items_o() {
+      return this.arr.filter(item => item.id < 3);
+    },
+    big_items_o() {
+      return this.arr.filter(item => item.id >= 3);
+    },
+  },
+  setup() {
+    const arr = reactive([
+        { id: 1, text: 'option item 1' },
+        { id: 2, text: 'option item 2' },
+        { id: 3, text: 'option item 3' },
+        { id: 4, text: 'option item 4' },
+        { id: 5, text: 'option item 5' },
+      ]);
+
+      const small_items_c = computed(() => {
+        return arr.filter(item => item.id < 3);
+      });
+
+      const big_items_c = computed(() => {
+        return arr.filter(item => item.id >= 3);
+      });
+
+      return {
+        small_items_c,
+        big_items_c,
+      };
+  }
+};
+</script>
---
+++ src/components/VFor.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="width: 200px">
+    <ol>
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ol>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive } from 'vue';
+
+export default {
+  setup() {
+    const items = reactive(['item-1', 'item-2', 'item-3']);
+
+    return {
+      items,
+    };
+  }
+}
+
+</script>
