4f52ce70d61852e019c32ac72c771673fac14a80
HanseungYoo <hanseung.yoo@hanwha.com>
2022-07-20 00:43:25 +0900

    day3: multi model props

---
+++ src/components/Multimodel.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ `[${msg}] : ${msg2}` }}</p>
+  <MultimodelChild v-model:msg="msg" v-model:msg2="msg2" />
+</template>
+
+<script lang="ts">
+import MultimodelChild from './MultimodelChild.vue';
+
+export default {
+  data() {
+    return {
+      msg: '초기 메세지1',
+      msg2: '초기 메세지2',
+    };
+  },
+  components: {
+    MultimodelChild
+  },
+};
+</script>
---
+++ src/components/MultimodelChild.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>메세지1 :
+      <input :value="msg" @input="$emit('update:msg', $event?.target?.value)" />
+    </div>
+    <div>메세지2 :
+      <input :value="msg2" @input="$emit('update:msg2', $event?.target?.value)" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['msg', 'msg2'],
+  emits: ['update:msg', 'update:msg2'],
+};
+</script>
